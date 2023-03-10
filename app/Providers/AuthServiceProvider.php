cb3ee57d39d5f41ab172efc19978a803ff4f6cea
Caique Daniel <caiquedaniel@outlook.com.br>
2023-03-10 20:21:14 -0300

    Configurar Guard JWT

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,16 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+use SebastianBergmann\ObjectReflector\Exception;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,7 +32,21 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        $this->registerJWTAuth();
+    }
 
-        //
+    private function registerJWTAuth()
+    {
+        Auth::viaRequest('jwt', function (Request $request) {
+            try {
+                $tokenPayload = JWT::decode($request->bearerToken(), new Key(config('jwt.key'), 'HS256'));
+
+                return User::find($tokenPayload)->first();
+            } catch (Exception $e) {
+                Log::error($e);
+
+                return null;
+            }
+        });
     }
 }
